94e17d7e4d96bfafe9c560ea03c8c2eced378528
skpptf <62537172+skpptf@users.noreply.github.com>
2020-06-21 00:34:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -75,7 +75,7 @@ swapon /dev/sda2
 echo "Starting install.."
 echo "Installing Arch Linux, KDE with Konsole and Dolphin and GRUB2 as bootloader" 
 #pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel plasma konsole dolphin
-pacstrap /mnt base base-devel zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel lxde
+pacstrap /mnt base base-devel mc vim ranger zsh grml-zsh-config grub os-prober intel-ucode efibootmgr dosfstools freetype2 fuse2 mtools iw wpa_supplicant dialog xorg xorg-server xorg-xinit mesa xf86-video-intel lxde
 
 
 # Generate fstab
